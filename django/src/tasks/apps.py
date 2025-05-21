f5e63e00f61bb21499ffe1ec49680cbc16e08be6
mulchus <mulch@list.ru>
2025-05-21 20:42:24 +0300

    Update tasks beat_schedule.

--- django/src/tasks/apps.py
+++ django/src/tasks/apps.py
@@ -1,18 +1,17 @@
 from django.apps import AppConfig
 from django.conf import settings
-from celery import Celery
+from celery import current_app
 
 
 class TasksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tasks'
 
-    def ready(self):
-        from .tasks import send_task_reminders
-        from celery import current_app
+    @staticmethod
+    def ready():
         current_app.conf.beat_schedule = {
-            'send-task-reminders-every-hour': {
+            'send-task-reminders-every-second': {
                 'task': 'tasks.tasks.send_task_reminders',
-                'schedule': 3600.0,  # каждые 60 минут
+                'schedule': 1.0
             },
         }
--- django/src/tasks/tasks.py
+++ django/src/tasks/tasks.py
@@ -1,4 +1,5 @@
 from celery import shared_task
+from django.contrib.sessions.backends import db
 from django.core.mail import send_mail
 from django.utils import timezone
 from .models import Task
@@ -8,12 +9,16 @@ from .models import Task
 def send_task_reminders():
     now = timezone.now()
     tasks = Task.objects.filter(due_date__lte=now, completed=False)
+    print(f'FILTERED TASKS {tasks}')
     for task in tasks:
-        send_mail(
-            'Task Reminder',
-            f'Reminder: The task "{task.title}" is due.',
-            'from@example.com',
-            [task.user.email],
-            fail_silently=False,
-        )
-        print(f'Reminder: The task "{task.title}" is due.', [task.user.email])
+        # send_mail(
+        #     'Task Reminder',
+        #     f'Reminder: The task "{task.title}" is due.',
+        #     'from@example.com',
+        #     [task.user.email],
+        #     fail_silently=False,
+        # )
+        print(f'REMINDER: The task "{task.title}" is due.', [task.user.email])
+        with db.transaction.atomic():
+            task.completed = True
+            task.save()
